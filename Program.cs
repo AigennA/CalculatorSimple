0634065676911f77a2c0e0d49b1693ed6e3c18b2
Aigen80 <aomurm@gmail.com>
2025-09-16 09:45:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CalculatorMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Calculator!");
+            Console.WriteLine("Press 1 for subtraction");
+            Console.WriteLine("Press 2 for addition");
+            Console.WriteLine("Press 3 for multiplation");
+            Console.WriteLine("Press 4 for division");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+        }
+
+
+    }
+}
